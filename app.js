2507a64fe8105ce25106faffa1d08845e45e950b
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-14 10:54:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+//  Type Error
+// let message:string = "Hello World";
+// console.loge(message); //error!
+var message1 = "Hello World";
+console.log(message1); //correct
---
+++ app.ts
@@ -0,0 +1,7 @@
+//  Type Error
+let message:string = "Hello World";
+console.loge(message); //error!
+
+// Correct Version.. .
+let message:string = "Hello World";
+console.log(message); //correct
